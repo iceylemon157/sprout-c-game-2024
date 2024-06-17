a91d4da27655bf51fde1114b9b6c27206da32f3d
iceylemon157 <oosheepyerd79135@gmail.com>
2024-06-18 03:26:22 +0800

    fix: update PrintEvents function for complete output

--- cserver/GameController.cc
+++ cserver/GameController.cc
@@ -80,6 +80,7 @@ void GameController::PrintEvents() {
     cout << "--- Current Game State ---" << endl;
 
     cout << "Round: " << GameController::GetRound() << endl;
+    cout << "Recipe Mode: " << GameController::GetRecipeMode() << endl;
     cout << "Total Score: " << GameController::GetTotalScore() << endl;
 
     cout << "Player Position: (" << GameController::GetPlayerPosition().first << ", " << GameController::GetPlayerPosition().second << ")" << endl;
@@ -88,6 +89,17 @@ void GameController::PrintEvents() {
 
     cout << "New Order: ";
     GameController::PrintOrderInfo(GameController::GetNewOrder());
+
+    cout << "Order Delivered: ";
+    int orderID = GameController::GetOrderDelivered().first;
+    if (orderID == 0) {
+        cout << "None" << endl;
+    } else if (orderID == -1) {
+        cout << "Delivery Failed!" << endl;
+    } else {
+        cout << "OrderID: " << GameController::GetOrderDelivered().first << ", ";
+        cout << "Score: " << GameController::GetOrderDelivered().second << endl;
+    }
 
     cout << "Order List: " << endl;
     for (auto order : GameController::GetOrderList()) {
